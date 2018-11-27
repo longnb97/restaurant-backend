41d59cd77e514952f8b1c903e6e14cae17b8639b
balo11044 <balo11044@gmail.com>
2018-11-27 19:49:50 +0700

    sdsd

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors({credentials: true}))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://thefoodhouse.herokuapp.com");
     res.header("Access-Control-Allow-Headers", "https://thefoodhouse.herokuapp.com");
-    res.header("Access-Control-Allow-Credentials", "https://thefoodhouse.herokuapp.com");
+    res.header("Access-Control-Allow-Credentials", true);
     if (req.method === "OPTIONS") {
       res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
       return res.status(200).json();
