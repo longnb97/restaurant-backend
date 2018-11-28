b0d8409c7394999156ac3dad337e812d82bf7ab5
balo11044 <balo11044@gmail.com>
2018-11-28 10:45:02 +0700

    sdsd

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(function(req, res, next) {
     }
     //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', '*');
     res.header('Access-Control-Allow-Credentials', true);
     return next();
   });
